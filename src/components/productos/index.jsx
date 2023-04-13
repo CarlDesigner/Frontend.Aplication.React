487e6d5b34bc408acd862559d863b3bc4d8cd287
Carlos Fajardo <61030996+CarlDesigner@users.noreply.github.com>
2023-04-13 06:36:51 +0000

    REESTABLECIENDO PROYECTO

--- src/components/productos/index.jsx
+++
@@ -1,9 +0,0 @@
-function IndexProductos(props) {
-    return (
-        <div>
-            <h4>Productos</h4>
-        </div>
-    );
-}
-
-export default IndexProductos;
--- src/components/usuarios/index.jsx
+++
@@ -1,9 +0,0 @@
-function IndexUsuarios(props) {
-    return (
-        <div>
-            <h4>Usuarios</h4>
-        </div>
-    );
-}
-
-export default IndexUsuarios;
