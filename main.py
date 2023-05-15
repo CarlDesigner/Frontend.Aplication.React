e763db285f9bb2628e47991636adfb669c54a1aa
CarlDesigner <krlitosfajardo626@gmail.com>
2023-05-15 16:37:51 -0500

    Aplicacion completa

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-import string
-import secrets
-
-
-def generate_password(length=12):
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    while True:
-        password = "".join(secrets.choice(alphabet) for i in range(length))
-        if (
-            any(c.islower() for c in password)
-            and any(c.isupper() for c in password)
-            and any(c.isdigit() for c in password)
-            and any(c in string.punctuation for c in password)
-        ):
-            break
-    return password
-
-
-def main():
-    password = generate_password()
-    print("La contraseña generada es:", password)
-
+from ui.password_generator_ui import *
 
 if __name__ == "__main__":
-    main()
+    window.mainloop()
+
+
+# Logica medinate la consola
+# import string
+# import secrets
+#
+#
+# def generate_password(length=12):
+#    alphabet = string.ascii_letters + string.digits + string.punctuation
+#    while True:
+#        password = "".join(secrets.choice(alphabet) for i in range(length))
+#        if (
+#            any(c.islower() for c in password)
+#            and any(c.isupper() for c in password)
+#            and any(c.isdigit() for c in password)
+#            and any(c in string.punctuation for c in password)
+#        ):
+#            break
+#    return password
+#
+#
+# def main():
+#    password = generate_password()
+#    print("La contraseña generada es:", password)
+#
+#
+# if __name__ == "__main__":
+#    main()
--- ui/password_generator_ui.py
+++ ui/password_generator_ui.py
@@ -26,11 +26,18 @@ def generate_password_button():
 def copy_password():
     password = password_label.cget("text")
     pyperclip.copy(password)
+    feedback_label.config(text="Contraseña copiada")
+    window.after(3000, clear_feedback)
+
+
+def clear_feedback():
+    feedback_label.config(text="")
 
 
 # Crear ventana
 window = tk.Tk()
 window.title("Generador de contraseñas")
+
 # Definir tamaño de la ventana
 window.geometry(
     "400x300+{}+{}".format(
@@ -39,17 +46,17 @@ window.geometry(
     )
 )
 
+# Crear etiqueta para mostrar la contraseña
+password_label = tk.Label(window, text="", font=("Arial", 12))
+password_label.pack(side=tk.TOP, pady=10)
+
+# Crear etiqueta para el mensaje de feedback
+feedback_label = tk.Label(window, text="", font=("Arial", 12))
+feedback_label.pack(side=tk.TOP)
+
 # Crear botón de copiar contraseña
 copy_button = tk.Button(window, text="Copiar contraseña", command=copy_password)
 copy_button.pack(pady=10)
-
-# Crear etiqueta para mostrar la contraseña
-password_label = tk.Label(window, text="", font=("Arial", 12))
-
-# Agregar fuente y tamaño
-password_label.pack(
-    side=tk.TOP, pady=10
-)  # Colocar la etiqueta arriba de todo, con un margen inferior
 
 # Crear botón para generar la contraseña
 generate_button = tk.Button(
@@ -58,10 +65,76 @@ generate_button = tk.Button(
     font=("Arial", 12),
     command=generate_password_button,
 )
-
-
-# Colocar el botón debajo de todo, con un margen superior
 generate_button.pack(side=tk.BOTTOM, pady=10)
 
 # Mostrar ventana
 window.mainloop()
+
+
+# import string
+# import secrets
+# import tkinter as tk
+# import pyperclip
+
+
+# def generate_password(length=10):
+#    alphabet = string.ascii_letters + string.digits + string.punctuation
+#    while True:
+#        password = "".join(secrets.choice(alphabet) for i in range(length))
+#        if (
+#            any(c.islower() for c in password)
+#            and any(c.isupper() for c in password)
+#            and any(c.isdigit() for c in password)
+#            and any(c in string.punctuation for c in password)
+#        ):
+#            break
+#    return password
+
+
+# def generate_password_button():
+#    password = generate_password()
+#    password_label.config(text=password)
+
+
+# def copy_password():
+#    password = password_label.cget("text")
+#    pyperclip.copy(password)
+
+
+# Crear ventana
+# window = tk.Tk()
+# window.title("Generador de contraseñas")
+# Definir tamaño de la ventana
+# window.geometry(
+#    "400x300+{}+{}".format(
+#        int((window.winfo_screenwidth() - 400) / 2),
+#        int((window.winfo_screenheight() - 300) / 2),
+#    )
+# )
+
+# Crear botón de copiar contraseña
+# copy_button = tk.Button(window, text="Copiar contraseña", command=copy_password)
+# copy_button.pack(pady=10)
+
+# Crear etiqueta para mostrar la contraseña
+# password_label = tk.Label(window, text="", font=("Arial", 12))
+
+# Agregar fuente y tamaño
+# password_label.pack(
+#    side=tk.TOP, pady=10
+# )  # Colocar la etiqueta arriba de todo, con un margen inferior
+
+# Crear botón para generar la contraseña
+# generate_button = tk.Button(
+#    window,
+#    text="Generar contraseña",
+#    font=("Arial", 12),
+#    command=generate_password_button,
+# )
+
+
+# Colocar el botón debajo de todo, con un margen superior
+# generate_button.pack(side=tk.BOTTOM, pady=10)
+
+# Mostrar ventana
+# window.mainloop()
